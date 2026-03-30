acdbb3868f8839b578d21ef591fdc2566acb8354
sahilvishwakarma79222 <vsahil79222@gmail.com>
2026-03-30 13:00:25 +0530

    solve dockerfile

--- src/main/java/com/alibou/websocket/controller/HomeController.java
+++ src/main/java/com/alibou/websocket/controller/HomeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
